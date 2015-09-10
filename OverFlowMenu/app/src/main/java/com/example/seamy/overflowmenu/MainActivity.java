d840169c48694adf2c62423f4ed11eb91407760a
Seamus O Connor <seamo123@gmail.com>
2015-09-10 13:09:54 +0900

    1 to 38 tutorials

---
+++ OverFlowMenu/app/src/main/java/com/example/seamy/overflowmenu/MainActivity.java
@@ -0,0 +1,71 @@
+package com.example.seamy.overflowmenu;
+
+import android.app.Activity;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.RelativeLayout;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+
+
+        RelativeLayout main_view = (RelativeLayout) findViewById(R.id.main_view);
+
+        switch (item.getItemId()) {
+            case R.id.menu_red:
+                if (item.isChecked())
+                    item.setChecked(false);
+                else
+                    item.setChecked(true);
+                main_view.setBackgroundColor(Color.RED);
+                return true;
+
+            case R.id.menu_green:
+                if (item.isChecked())
+                    item.setChecked(false);
+                else
+                    item.setChecked(true);
+                main_view.setBackgroundColor(Color.GREEN);
+                return true;
+
+
+            case R.id.menu_yellow:
+                if (item.isChecked())
+                    item.setChecked(false);
+                else
+                    item.setChecked(true);
+                main_view.setBackgroundColor(Color.YELLOW);
+                return true;
+
+
+            default:
+               return super.onOptionsItemSelected(item);
+
+        }
+
+    }
+}
+
+
+
+
+
+
